1026c37bbb08208efbd3b285ed47b8f2f8881f38
SweetPicklee <francisbruceclark1998@gmail.com>
2019-03-20 04:03:17 +0000

    Added html css and js files

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ var express = require('express'),
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 
 app.get('/', (req, res) => {
-    res.send('hi');
+    res.sendFile('index.html');
 })
 
 app.use('/api/todos', todoRoutes)
